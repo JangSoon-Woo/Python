58dda1d748b5ce8dbb9182c662a9e94020544951
unknown <sdh190419@sdh.hs.kr>
2021-05-24 07:37:57 +0900

    Python

--- Foundation.py
+++ Foundation.py
@@ -1,3 +1,12 @@
+# 
+"""
+주석
+주석
+주석
+"""
+
+# 1
+
 print("Hello, World!")
 
 # 2
@@ -8,5 +17,29 @@ if 5 > 2:
 # 3
 x = 4
 y = "test"
+z = 1j
 print(x)
 print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4
+
+import random
+
+print(random.randrange(1, 100))
+
+# 5
+
+a = "Hello"
+
+print(a)
+print(type(a))
+
+b = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(b)
