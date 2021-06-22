da055a9f119e1e72547b73ed00b6a47f7e68229b
unknown <sdh190419@sdh.hs.kr>
2021-06-23 07:07:40 +0900

    Python

--- while/1110.py
+++ while/1110.py
@@ -5,7 +5,9 @@ temp = 0
 count = 0
 while True:
     temp = num//10 + num%10
-    new_num
+    new_num = (num%10)*10 + temp%10
+    count += 1
+    num = new_num
     if new_num == check:
         break
 print(count)
