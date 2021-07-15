a5309f3303cd5335bd73c0b8bd1e5485248f10fd
jojem096 <sdh190419@sdh.hs.kr>
2021-07-16 00:03:13 +0900

    Python

--- 1100.py
+++ 1100.py
@@ -2,10 +2,10 @@ chess = []
 for _ in range(8):
     chess.append(list(map(str, list(input()))))
 
-aw = 0
+answer = 0
 for i in range(8):
     for j in range(8):
         if (i + j) % 2 == 0: #하얀칸일 경우
-            
-print(aw)
-# 미완
+            if chess[i][j] == 'F': #F있을 경우
+                answer += 1
+print(answer)
